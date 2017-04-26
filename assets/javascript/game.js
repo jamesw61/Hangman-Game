1e996573e807f3818cee9ab5e53265bed413e129
James Wick <jmwick2@hotmail.com>
2017-04-25 20:06:57 -0700

    added guess counter and game start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,77 @@
 var mainWord = generateWord();
-var undersc = hideWord(mainWord);
-document.getElementById("displayWord").innerHTML = undersc;
- 
-console.log(mainWord);
+var underScores = hideWord(mainWord);
 
+var tries = 5;
+var guesses = 0;
+
+startover();
+
+
+function startover()
+  {
+    document.onkeyup = function(event)
+    {
+      playgame();
+    }
+  }
+
+
+function playgame()
+  {
+    document.getElementById("displayWord").innerHTML = underScores;
+    document.getElementById("instruction").innerHTML = "Guess a Letter";
+    document.onkeyup = function(event)
+    {
+      var letterGuessed = event.key;
+      document.getElementById("displayLetter").innerHTML = letterGuessed.toUpperCase();
+      replaceLetter(letterGuessed);
+    }
+  }
 
 
 
 function generateWord()
   {
-  var wordlist = ["concatenation", "polymorphism", "encapsulation"];
-  var index = Math.floor(Math.random() * wordlist.length);
-  return wordlist[index];
+    var wordlist = ["concatenation", "polymorphism", "encapsulation"];
+    var index = Math.floor(Math.random() * wordlist.length);
+    return wordlist[index];
   }
-
-        
         
 function hideWord(word)
   {   
-  var hiddenWord = "";
-  for (var i=0; i < word.length; i++)
-    {
-    hiddenWord = hiddenWord + "_";
-    }
-  return hiddenWord;
+    var hiddenWord = "";
+    for (var i=0; i < word.length; i++)
+      {
+       hiddenWord = hiddenWord + "_";
+      }
+        return hiddenWord;
   }
-
-  document.onkeyup = function(event)
-    {
-    letterGuessed = event.key;
-    document.getElementById("displayLetter").innerHTML = letterGuessed;
-    replaceLetter(letterGuessed);
-    }
-
-   
 
 function replaceLetter(y)
   {
-        y = y.toLowerCase();    
+    y = y.toLowerCase();    
         
-        var underscArray = undersc.split("");
-        console.log("underscarray split " + underscArray);
-       
-        var mainWordArray = mainWord.split("");
-        console.log("mainword split   " + mainWordArray);
-        console.log("---" + mainWordArray[3]);
-
-        for (var j=0; j < mainWord.length; j++) {
-            
-            console.log("aa" + mainWordArray[j]);
-            var x = mainWordArray[j];
-            console.log("***  " + x + "  " + y);
-          if(x === y) {
-            console.log("bb" + mainWordArray[j]);
-            underscArray[j] = y;
-            // console.log("if " + underscArray);
-            console.log("cc" + mainWordArray[j]);
-            console.log(y);
-          }
-
+    var underscArray = underScores.split("");
+              
+    var mainWordArray = mainWord.split("");
+        
+    for (var j=0; j < mainWord.length; j++) 
+      {
+        var x = mainWordArray[j];
+        if(x === y) 
+        {
+          underscArray[j] = y;
+          
         }
-        // console.log(wordArray);
-        
-        // console.log(wordArray);
-        var newHidden = underscArray.join("");
-        document.getElementById("displayWord").innerHTML = newHidden.toUpperCase();
-        undersc = newHidden
+      }
+    
+    var newHidden = underscArray.join("");
+    if(underScores === newHidden)
+    {
+      guesses++;
+    }
+    document.getElementById("displayWord").innerHTML = newHidden.toUpperCase();
+    document.getElementById("numberOfMisses").innerHTML = "Number of Missed Letters: " + guesses;
+    underScores = newHidden;
   }
   
